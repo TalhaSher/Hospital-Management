594b6e3f2e9e4f36881446e983067324d1d07c4e
Talha Sher <xexcalibur9090@gmail.com>
2024-05-08 23:36:40 +0500

    add doctor sign in

--- server/controllers/doctor.js
+++ server/controllers/doctor.js
@@ -1,1 +1,20 @@
 import Doctor from "../models/Doctor";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+// SIGN IN
+
+const doctorSignIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const doctor = await Doctor.findOne({ email: email });
+    if (!doctor)
+      return res.status(400).json({ msg: "Doctor Account Not Found !" });
+    const isMatch = await bcrypt.compare(password, doctor.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials." });
+
+    const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET);
+    res.status(200).json({ token, doctor });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
